bb3b017e82220fc6e15f3284e1ede3ada3b8ce4d
Rodrigo Monteiro <63541433+caprica64@users.noreply.github.com>
2021-09-22 17:20:47 -0300

    Added a new variable to VPC Name declared

--- variables.tf
+++ variables.tf
@@ -3,3 +3,9 @@ variable "region" {
   type = string
   description = "AWS region to use for this project"
 }
+
+variable "vpc_name" {
+  default = "VPCName"
+  type = string
+  description = "VPC name for this lab"
+}
