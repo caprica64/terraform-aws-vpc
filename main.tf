b454b8eac0423bde1b1bfc27670f8e8e99002b5f
Rodrigo Monteiro <63541433+caprica64@users.noreply.github.com>
2021-09-22 15:42:26 -0300

    Restart new VPC using Terraform modules

--- main.tf
+++ main.tf
@@ -1,40 +1,19 @@
-provider "aws" {
-  region = local.region
-}
-
-locals {
-  region = "eu-west-1"
-}
-
-################################################################################
-# VPC Module
-################################################################################
-
 module "vpc" {
-  source = "../../"
+  source = "terraform-aws-modules/vpc/aws"
 
-  name = "simple-example"
+  name = "my-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["${local.region}a", "${local.region}b", "${local.region}c"]
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_ipv6 = true
-
-  enable_nat_gateway = false
-  single_nat_gateway = true
-
-  public_subnet_tags = {
-    Name = "overridden-name-public"
-  }
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
 
   tags = {
-    Owner       = "user"
+    Terraform = "true"
     Environment = "dev"
   }
-
-  vpc_tags = {
-    Name = "vpc-name"
-  }
 }
+  
